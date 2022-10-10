e53b8c1e33831faae9d293c8f0a4a57416e54300
lisa-go <lis-zaa@live.com>
2022-10-09 21:42:06 -0500

    able to adjust box size to fit any amount of boxes and fit in container

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 
     //...... selecting DOM parent ......//
-    const container = document.querySelector("#gridcontainer");
+    const gridcontainer = document.querySelector("#gridcontainer");
+
+    //...... determine amount of boxes in each row and column ......//
+    let boxAmount = 64;
 
     //...... looping create divbox ......//
-    for (let i = 0; i < (16 * 16); i++) {
+    for (let i = 0; i < (boxAmount * boxAmount); i++) {
 const box = document.createElement("div");
 box.classList.add("box");
-container.appendChild(box);
 
+    //...... calculating pixel size of each box to fit container size ......//
+let boxSize = 960 / boxAmount;
+box.style.width = boxSize + "px";
+box.style.height = boxSize + "px";
+
+gridcontainer.appendChild(box);
+
+    //...... mouseover box to change color ......//
 box.addEventListener("mouseover", () => {
     box.style.backgroundColor = "pink";
 });
