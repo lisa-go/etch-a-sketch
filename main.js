9d514bfa4316c1286e527ababde0841dc965dd13
lisa-go <lis-zaa@live.com>
2022-10-09 23:08:26 -0500

    added a button promptfor user to input grid size and resets container before generating new grid

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 
     //...... selecting DOM parent ......//
-    const gridcontainer = document.querySelector("#gridcontainer");
+    const gridcontainer = document.querySelector("#gridcontainer");      
 
     //...... determine amount of boxes in each row and column ......//
-    let boxAmount = 64;
+function setGridsize() {
+    resetGrid();
+    let gridSize = prompt("Set the size of the grid (max. 100)");
+    
+    if (gridSize > 100) {
+        alert("invalid");
+        return;
+    }
 
     //...... looping create divbox ......//
-    for (let i = 0; i < (boxAmount * boxAmount); i++) {
+    for (let i = 0; i < (gridSize * gridSize); i++) {
 const box = document.createElement("div");
 box.classList.add("box");
 
     //...... calculating pixel size of each box to fit container size ......//
-let boxSize = 960 / boxAmount;
+let boxSize = 800 / gridSize;
 box.style.width = boxSize + "px";
 box.style.height = boxSize + "px";
+
 
 gridcontainer.appendChild(box);
 
@@ -21,6 +29,13 @@ gridcontainer.appendChild(box);
 box.addEventListener("mouseover", () => {
     box.style.backgroundColor = "pink";
 });
+    } 
+}
+
+    //...... function to reset existing grid ......//
+function resetGrid() {
+    let element = document.getElementById("gridcontainer");
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
     }
-
-
+}
