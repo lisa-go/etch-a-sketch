646f8db09cdee25eb94cb09c49e01582b495ecab
lisa-go <lis-zaa@live.com>
2022-10-10 21:17:02 -0500

    random color on hover

--- main.js
+++ main.js
@@ -2,32 +2,64 @@
     //...... selecting DOM parent ......//
     const gridcontainer = document.querySelector("#gridcontainer");      
 
+    //...... random color......//
+    const getRandomNumber = (maxNum) => {
+        return Math.floor(Math.random() * maxNum);
+      };
+      const getRandomColor = () => {
+        const r = getRandomNumber(256);
+        const g = getRandomNumber(256);
+        const b = getRandomNumber(256);
+       
+        return `rgb(${r}, ${g}, ${b})`;
+      };
+      const setBackgroundColor = () => {
+        const randomColor = getRandomColor();
+        document.getElementsByClassName("box").style.backgroundColor = randomColor;
+    }
+    
+
+    //...... set default grid ......//
+function defaultGrid() {
+     for (let i = 0; i < (16 * 16); i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        let boxSize = 800 / 16;
+        box.style.width = boxSize + "px";
+        box.style.height = boxSize + "px";
+        gridcontainer.appendChild(box);
+            box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = getRandomColor();
+        });
+            } 
+}
+
+
     //...... determine amount of boxes in each row and column ......//
 function setGridsize() {
     resetGrid();
-    let gridSize = prompt("Set the size of the grid (max. 100)");
-    
-    if (gridSize > 100) {
+    gridSize = prompt("Set the size of the grid (max. 100)");
+       if (gridSize > 100) {
         alert("invalid");
         return;
     }
 
     //...... looping create divbox ......//
-    for (let i = 0; i < (gridSize * gridSize); i++) {
-const box = document.createElement("div");
-box.classList.add("box");
+        for (let i = 0; i < (gridSize * gridSize); i++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
 
     //...... calculating pixel size of each box to fit container size ......//
-let boxSize = 800 / gridSize;
-box.style.width = boxSize + "px";
-box.style.height = boxSize + "px";
+        let boxSize = 800 / gridSize;
+        box.style.width = boxSize + "px";
+        box.style.height = boxSize + "px";
 
 
-gridcontainer.appendChild(box);
+    gridcontainer.appendChild(box);
 
     //...... mouseover box to change color ......//
-box.addEventListener("mouseover", () => {
-    box.style.backgroundColor = "pink";
+        box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = getRandomColor();
 });
     } 
 }
@@ -39,3 +71,7 @@ function resetGrid() {
       element.removeChild(element.firstChild);
     }
 }
+
+
+
+defaultGrid();
