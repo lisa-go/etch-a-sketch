b30be343e4ac055e21706cc01582980582bc1142
lisa-go <lis-zaa@live.com>
2022-10-11 02:23:09 -0500

    added several buttons, random color, black, shading, erase and clear all

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
 
     //...... selecting DOM parent ......//
     const gridcontainer = document.querySelector("#gridcontainer");      
+    let color = "black";
+    let click = false;
 
-    //...... random color......//
-    const getRandomNumber = (maxNum) => {
-        return Math.floor(Math.random() * maxNum);
-      };
-      const getRandomColor = () => {
-        const r = getRandomNumber(256);
-        const g = getRandomNumber(256);
-        const b = getRandomNumber(256);
-       
-        return `rgb(${r}, ${g}, ${b})`;
-      };
-      const setBackgroundColor = () => {
-        const randomColor = getRandomColor();
-        document.getElementsByClassName("box").style.backgroundColor = randomColor;
-    }
-    
 
     //...... set default grid ......//
-function defaultGrid() {
-     for (let i = 0; i < (16 * 16); i++) {
+function defaultGrid(num) {
+     for (let i = 0; i < (num * num); i++) {
         const box = document.createElement("div");
         box.classList.add("box");
-        let boxSize = 800 / 16;
+        let boxSize = 800 / num;
         box.style.width = boxSize + "px";
         box.style.height = boxSize + "px";
         gridcontainer.appendChild(box);
-            box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = getRandomColor();
-        });
-            } 
-}
+            box.addEventListener("mouseover", colorGrid);
+        };
+    } 
 
 
     //...... determine amount of boxes in each row and column ......//
@@ -57,12 +41,58 @@ function setGridsize() {
 
     gridcontainer.appendChild(box);
 
-    //...... mouseover box to change color ......//
-        box.addEventListener("mouseover", () => {
-        box.style.backgroundColor = getRandomColor();
-});
+    //...... click mouseover box to change color ......//
+    box.addEventListener("mouseover", colorGrid);
     } 
 }
+    
+    document.querySelector('body').addEventListener('mouseup', (e) =>{
+        click = false;
+    });
+    document.querySelector('body').addEventListener('mousedown', (e) =>{
+        if (e.target.tagname != "BUTTON"){
+        click = true;
+        }
+    });
+
+
+    //...... random color......//
+    const getRandomNumber = (maxNum) => {
+        return Math.floor(Math.random() * maxNum);
+      };
+      const getRandomColor = () => {
+        const r = getRandomNumber(256);
+        const g = getRandomNumber(256);
+        const b = getRandomNumber(256);
+       
+        return `rgb(${r}, ${g}, ${b})`;
+      };
+      const setBackgroundColor = () => {
+        color = getRandomColor();
+        box.style.backgroundColor = color;
+    }
+    
+
+    //...... color switching between rainbow and black ......//
+
+function changeColor(choice) {
+    color = choice;
+}
+
+    //...... coloring the grid ......//
+function colorGrid() {
+    if (click) {
+        if((color == "random")) {
+        this.style.backgroundColor = getRandomColor();
+        }
+        if ((color == "shade")) {
+            this.style.backgroundColor = "black";
+            this.style.opacity = (parseFloat(this.style.opacity) || 0) + 0.1;
+        }
+        this.style.backgroundColor = color;
+    }
+}
+
 
     //...... function to reset existing grid ......//
 function resetGrid() {
@@ -72,6 +102,16 @@ function resetGrid() {
     }
 }
 
+    //...... function to set all boxes back to white......//
+function clearGrid() {
+    let nodes = document.getElementById('gridcontainer').childNodes;
+    for(let i=0; i<nodes.length; i++) {
+        if (nodes[i].nodeName.toLowerCase() == 'div') {
+             nodes[i].style.background = "white";
+             nodes[i].style.opacity = 1;
+         }
+    }
+}
 
 
-defaultGrid();
+defaultGrid(16);
